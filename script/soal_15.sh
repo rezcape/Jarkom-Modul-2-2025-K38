ff822c9986e66ce1ae243dfd73498973343cf42a
Putri Joselina Silitonga <pjsilitongaa@gmail.com>
2025-10-13 16:21:10 +0700

    Update soal_15.sh

--- script/soal_15.sh
+++ script/soal_15.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# Di Elrond:
+
+# 1. Test koneksi
+curl http://www.k38.com/app/
+curl http://www.k38.com/static/
+
+# 2. Load testing
+ab -n 500 -c 10 http://www.k38.com/app/
+ab -n 500 -c 10 http://www.k38.com/static/
+Di Sirion (untuk troubleshooting):
+bash
+# Cek nginx process
+ps aux | grep nginx
+
+# Test config
+nginx -t
+
+# Cek error log
+tail -f /var/log/nginx/error.log
+Di Lindon & Vingilot (untuk troubleshooting):
+
+# Cek service running
+ps aux | grep nginx
+ps aux | grep php
+
+# Cek error log
+tail -f /var/log/nginx/error.log
